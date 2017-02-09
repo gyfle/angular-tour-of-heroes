782aa947aa3389d129ba36cbae1e31b7a901ff20
gyfle <smilmere@gmail.com>
2017-02-09 13:26:00 +0100

    Routing part 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes.component';
@@ -10,7 +11,13 @@ import { HeroDetailComponent } from './hero-detail.component';
 @NgModule({
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot([
+      {
+        path: 'heroes',
+        component: HeroesComponent
+      }
+    ])
   ],
   declarations: [
     AppComponent,
@@ -23,3 +30,4 @@ import { HeroDetailComponent } from './hero-detail.component';
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
